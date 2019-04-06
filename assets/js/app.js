9a84c58439ec42f2c724ea726a068b5603618bf5
João Soares <jsoares.public@gmail.com>
2019-04-06 19:04:16 +0100

    he

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,36 @@
+const vals = [
+    {
+        icon: 'fa-laugh-wink',
+        info: 'You\'re looking healthy to me!',
+        top: '80%',
+        color: '#45DE86',
+        textcolor: '#45DE86',
+        bpm: 72,
+        alcohol: '0.02%',
+        oxygen: '87%'
+    },
+    {
+        info: 'You should be more careful.',
+        icon: 'fa-frown-open',
+        top: '45%',
+        color: '#e8c34f',
+        textcolor: '#fcffae',
+        bpm: 90,
+        alcohol: '1.30%',
+        oxygen: '70%'
+    },
+    {
+        info: 'You\'re not fine! Calling help!',
+        icon: 'fa-ambulance',
+        top: '25%',
+        color: '#b90808',
+        textcolor: '#f78b75',
+        bpm: 130,
+        alcohol: '3.20%',
+        oxygen: '50%'
+    }
+]
+
 // direction = -1 -> left | direction = 1 -> right
 function moveWidget(direction) {
     let ws = document.getElementById('widget-strip')
@@ -41,4 +74,41 @@ function startClock() {
     document.getElementById('clock-date').innerHTML = weekday + ', ' + day + ' ' + month
 
     setTimeout(startClock, 1000)
+}
+
+function randomizeHealthInfo() {
+    let i = Math.floor(Math.random() * 3)
+    let val = vals[i]
+    let icon = document.getElementById('status-icon')
+    let info = document.getElementById('status-info')
+    let waves = document.getElementsByClassName('wave')
+    let bpm = document.getElementById('bpm')
+    let oxygen = document.getElementById('oxygen')
+    let alcohol = document.getElementById('alcohol')
+
+    if(icon) {
+        icon.classList.add(val.icon)
+        icon.style.color = val.textcolor
+    }
+
+    if(info) {
+        info.innerHTML = val.info
+    }
+
+    if(bpm) {
+        bpm.innerHTML = val.bpm
+    }
+
+    if(oxygen) {
+        oxygen.innerHTML = val.oxygen
+    }
+
+    if(alcohol) {
+        alcohol.innerHTML = val.alcohol
+    }
+    
+    Array.from(waves).forEach(wave => {
+        wave.style.top = val.top
+        wave.style.background = val.color
+    })
 }
