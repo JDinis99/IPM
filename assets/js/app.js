fd950c754160b790a3b3a121700e88fd9ecaadd2
João Soares <jsoares.public@gmail.com>
2019-04-20 10:43:08 +0100

    added skycons

--- assets/js/app.js
+++ assets/js/app.js
@@ -227,3 +227,57 @@ function updateNotifications() {
         notification_btn.classList.remove('notification-bounce')
     }
 }
+
+const weather_vals = [
+    {
+        icon: "clear-day",
+        temp: 26
+    },
+    {
+        icon: "clear-night",
+        temp: 21
+    },
+    {
+        icon: "partly-cloudy-day",
+        temp: 20
+    },
+    {
+        icon: "partly-cloudy-night",
+        temp: 17
+    },
+    {
+        icon: "cloudy",
+        temp: 15
+    },
+    {
+        icon: "rain",
+        temp: 13
+    },
+    {
+        icon: "sleet",
+        temp: 14
+    },
+    {
+        icon: "snow",
+        temp: 2
+    },
+    {
+        icon: "wind",
+        temp: 7
+    },
+    {
+        icon: "fog",
+        temp: 12
+    }
+]
+
+if(document.getElementById('weather-icon')) {
+    let i = Math.floor(Math.random() * weather_vals.length)
+
+    let skycons = new Skycons({"color": "#271f30"});
+    skycons.add("weather-icon", weather_vals[i].icon);
+    skycons.play();
+
+    let temp = document.getElementById('weather-temp')
+    temp.innerHTML = weather_vals[i].temp
+}
